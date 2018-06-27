667575c4d67205c14840cb101987626a3ce5e630
Alvio Wayne <alviowayne@gmail.com>
2018-06-27 19:03:28 +0300

    initial commit

---
+++ AWITCore/AWIT.Core6.config.js
@@ -0,0 +1,13 @@
+//	true - on, false - off,
+$AWITCore({
+	Init: true,
+	BaseStyle: true,
+	CustomStylesScripts: ['pages.css'],
+	StyleVer: '1',
+	WowNew: true,
+	ImgAfterLoad: true,
+	BodyAbsoluteCenter: true,
+	
+//	EXPERIMENTAL
+	GPUAcseleration: true,
+});
---
+++ AWITCore/style_custom/particleground/particleground.js
@@ -0,0 +1,12 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+*/
+// jQuery plugin example:
+$(function() {
+	$('#particles').particleground({
+		dotColor: 'rgba(65, 131, 196, 0.1)',
+		lineColor: 'rgba(65, 131, 196, 0.1)'
+	});
+});
+
